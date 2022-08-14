8a4cdde763be37d4d97dad45822d26bcc449a3a2
hrom80 <soldatovmark@gmail.com>
2022-08-14 12:52:08 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -12,11 +12,11 @@ TARGET_SCREEN_WIDTH := 1920
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/haier/rk3188/device_rk3188.mk)
+$(call inherit-product, device/rockchip/rk3188/device_rk3188.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rk3188
 PRODUCT_NAME := cm_rk3188
-PRODUCT_BRAND := haier
+PRODUCT_BRAND := Q7
 PRODUCT_MODEL := rk3188
-PRODUCT_MANUFACTURER := haier
+PRODUCT_MANUFACTURER := Q7
